a1f2ebd57a3d535f415a58135297654bd26fddd6
Cyara <cmyarap@gmail.com>
2025-08-07 23:09:03 -0500

    Se devuelven cambios

--- services/auditoriaService.py
+++ services/auditoriaService.py
@@ -5,7 +5,6 @@ from datetime import datetime
 from flask import Response
 from models import respuesta_log
 import re
-import regex
 import requests
 from pytz import timezone, utc
 from datetime import datetime
@@ -18,8 +17,8 @@ from functools import wraps
 MIME_TYPE_JSON = "application/json"
 STATUS_BAD_REQUEST = "Bad Request"
 STATUS_SUCCESS = "Successful request"
-PATRON = regex.compile(r"\[([^\]]*)\] - ([^\n]+)")  # Más específica y segura
-ANSI_ESCAPE = regex.compile(r'\x1B[@-Z\\-_]|\x1B\[[0-?]*[ -/]*[@-~]')  # Optimizada
+PATRON = re.compile(r"\[(.*?)\] - (.+)")
+ANSI_ESCAPE = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
 ERROR_WSO2_SIN_USUARIO = "Error WSO2 - Sin usuario"
 USUARIO_NO_REGISTRADO = "Usuario no registrado"
 NOMBRE_NO_ENCONTRADO = "Nombre no encontrado"
